8308ef939a38650603d24fe43cd086cf8a5c014d
MOgorodnik <mr.ogorodnik@gmail.com>
2024-07-21 22:32:23 +0300

    added error hadlers and alerts on remove and add product actions

--- src/components/ProductsItems.vue
+++ src/components/ProductsItems.vue
@@ -1,13 +1,14 @@
 <!-- components/ProductsItems.vue -->
 <script setup lang="ts">
 import { useProductStore } from "@/store";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import ProductItem from './ProductItem.vue';
 import AddProductForm from "./AddProductForm.vue";
 
 import type { Product } from '@/store';
 
 const productsStore = useProductStore();
+const error = ref<string | null>(null);
 
 const sortedProducts = computed(() => productsStore.sortedProducts);
 
@@ -15,12 +16,29 @@ const toggleSort = () => {
   productsStore.setSortOrder(!productsStore.sortAscending);
 };
 
-const removeProduct = (productId: number) => {
-  productsStore.removeProduct(productId);
+// const removeProduct = (productId: number) => {
+//   productsStore.removeProduct(productId);
+// };
+
+// const handleAddedProduct = (newProduct: Product) => {
+//   productsStore.addProduct(newProduct);
+// };
+const removeProduct = async (productId: number) => {
+  try {
+    await productsStore.removeProduct(productId);
+    error.value = null;
+  } catch (err) {
+    error.value = 'Failed to remove product. Please try again.';
+  }
 };
 
-const handleAddedProduct = (newProduct: Product) => {
-  productsStore.addProduct(newProduct);
+const handleAddedProduct = async (newProduct: Product) => {
+  try {
+    await productsStore.addProduct(newProduct);
+    error.value = null;
+  } catch (err) {
+    error.value = 'Failed to add product. Please try again.';
+  }
 };
 </script>
 
@@ -28,9 +46,12 @@ const handleAddedProduct = (newProduct: Product) => {
   <div>
     <h2>Products list </h2>
 
+    <div v-if="error" class="alert alert-danger">{{ error }}</div>
+
     <div v-if="sortedProducts.length === 0" class="alert alert-info">
       No products available.
     </div>
+    
     <div v-else>
       <AddProductForm @added-product="handleAddedProduct" />
 
